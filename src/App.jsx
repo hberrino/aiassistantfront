2b656bb1e330464e84c82de622051db33b394436
hberrino <berrinohernan@gmail.com>
2026-02-23 14:59:08 -0300

    pathing

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     setAnalysis(null)
 
     try {
-      const response = await fetch('http://localhost:8080/api/recruiter/analyze', {
+      const response = await fetch('https://asistenteai-rayq.onrender.com/api/recruiter/analyze', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
